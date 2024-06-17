568c27bb8a14117e826beef38937c35748270100
angelDAW1 <agsalamerobetancurt@iesmordefuentes.com>
2024-06-17 08:37:53 +0200

    Newpost fix for images

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"xN+tF6KUkVAEJtQvGZwbqi40x+qIyW55JhEdbx3nxTE=\"}"
--- .next/static/chunks/app/pages/layout-ecc5fbab24e279e1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4515],{1858:function(n,a,e){Promise.resolve().then(e.t.bind(e,58667,23))},58667:function(n){n.exports={style:{fontFamily:"'__Inter_aaf875', '__Inter_Fallback_aaf875'",fontStyle:"normal"},className:"__className_aaf875"}}},function(n){n.O(0,[3797,2971,7023,1744],function(){return n(n.s=1858)}),_N_E=n.O()}]);
